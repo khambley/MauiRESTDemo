879e2b2e057741b592e8bf5990dca8f90b22cd31
Katherine Hambley <superdesigngirl@mac.com>
2022-11-28 15:43:01 -0600

    Added GetUsers functionality

---
+++ MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using System.Windows.Input;
+
+namespace MauiRESTDemo
+{
+    public class MainViewModel
+    {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://6383d73e3fa7acb14fe8f6b2.mockapi.io";
+
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+        }
+        public ICommand AddUserCommand => new Command(async () =>
+        {
+            var url = $"{baseUrl}/users";
+
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //var content = await response.Content.ReadAsStringAsync();
+
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer.DeserializeAsync<List<User>>(responseStream, _serializerOptions);
+                }
+            }
+            // add an else to capture http errors, i.e. 500, log it, and display a message.
+        });
+    }
+}
+
---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MauiRESTDemo
+{
+	public class User
+	{
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Id { get; set; }
+    }
+}
+
