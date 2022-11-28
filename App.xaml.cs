dae64a7e466d316abcbacddccb2b5571ff9a839b
Katherine Hambley <superdesigngirl@mac.com>
2022-11-28 15:39:38 -0600

    Added initial project files

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiRESTDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiRESTDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
